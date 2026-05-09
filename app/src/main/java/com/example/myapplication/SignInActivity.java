ebb40b078c4d72ddfb3826c048577e713632dfa3
jamela <amerjamelahamer@gmail.com>
2026-05-09 13:22:48 +0300

    FistCommit

---
+++ app/src/main/java/com/example/myapplication/SignInActivity.java
@@ -0,0 +1,36 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SignInActivity extends AppCompatActivity {
+    private TextView signUpTV;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_sign_in);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        signUpTV = findViewById(R.id.sign_up_tv);
+        signUpTV.setOnClickListener(v -> {
+            Intent intent = new Intent(SignInActivity.this, SignUpaActivity.class);
+            startActivity(intent);
+        });
+        }
+
+
+
+
+}
